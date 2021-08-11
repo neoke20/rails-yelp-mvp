f1934e91037c16e65d1310845c8a17221dbbb32e
Kevin Konczak <konczakkevin@gmail.com>
2021-08-11 18:29:18 +0900

    completed the challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning Database'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+15.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORIES.sample
+  )
+  puts "Creating restaurant ##{restaurant.id}"
+end
+puts "Done!"
